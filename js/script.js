98f1cceeb7112bfe8e03eb1dfd3a46b67d7178e6
Cloudy Coder <kushalmaharjan472@gmail.com>
2026-04-07 19:54:32 +0545

    second commit

--- js/script.js
+++ js/script.js
@@ -48,3 +48,15 @@ function typeEffect() {
 }
 
 typeEffect();
+
+const faders = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, { threshold: 0.2 });
+
+faders.forEach(el => observer.observe(el));
